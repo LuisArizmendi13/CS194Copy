3b095bbfcf268cf807133a5ca259647d17e0db6f
LuisArizmendi13 <larizmen@stanford.edu>
2025-01-27 22:49:40 -0800

    wrote a dish and sale class

---
+++ website/src/classes/dish.js
@@ -0,0 +1,33 @@
+// Dish.js
+class Dish {
+    constructor(name, price, ingredients = []) {
+      this.name = name;
+      this.ingredients = ingredients; // List of ingredients for the dish
+      this.price = price;
+      this.sales = []; // List of sales made for this dish 
+      this.archive = false;
+    }
+  
+    made_sale(sale) {
+      this.sales.push(sale); // Add a sale to the list
+    }
+  
+    sale_count() {
+      return this.sales.length; // Return the number of sales
+    } 
+    archive_(){ 
+      this.archive = true;
+    }
+  }
+  
+  class Sale {
+    constructor(dish, time = new Date()) {
+      this.time = time; // Time sale was made
+      this.price = dish.price; // Price of the dish sold
+      this.dish_name = dish.name; // Name of the dish sold
+    }
+  }
+  
+  // Export the classes to be used in other files
+  module.exports = { Dish, Sale };
+  
