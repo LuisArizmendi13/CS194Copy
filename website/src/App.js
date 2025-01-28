d1c2c657b68e994daab7d613919a191c806d7d52
LuisArizmendi13 <larizmen@stanford.edu>
2025-01-27 23:14:24 -0800

    Add dishes page, with button

--- website/src/App.js
+++ website/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navbar } from './components/navbar';
+import { Navbar } from './components/navbar'; // Adjust path as needed
+import DishesPage from './components/DishesPage'; // Import the DishesPage component
 
 const App = () => (
   <Router>
@@ -9,6 +10,7 @@ const App = () => (
       <Route path="/" element={<div className="p-4">Welcome to the Home Page</div>} />
       <Route path="/page1" element={<div className="p-4">Welcome to Page 1</div>} />
       <Route path="/page2" element={<div className="p-4">Welcome to Page 2</div>} />
+      <Route path="/dishes" element={<DishesPage />} /> {/* New Route */}
     </Routes>
   </Router>
 );
