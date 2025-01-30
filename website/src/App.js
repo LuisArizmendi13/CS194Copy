fb95c82ca4155cda23de633f8de1379e263a2e02
alanaespo <146492343+alanaespo@users.noreply.github.com>
2025-01-30 08:26:30 -0800

    archive page and buggy calendar feature

--- website/src/App.js
+++ website/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/navbar'; // Adjust path as needed
 import DishesPage from './components/DishesPage'; // Import the DishesPage component
+import ArchivePage from './components/ArchivePage'; // Import the ArchivePage component
 
 const App = () => (
   <Router>
@@ -11,6 +12,7 @@ const App = () => (
       <Route path="/page1" element={<div className="p-4">Welcome to Page 1</div>} />
       <Route path="/page2" element={<div className="p-4">Welcome to Page 2</div>} />
       <Route path="/dishes" element={<DishesPage />} /> {/* New Route */}
+      <Route path="/archive" element={<ArchivePage />} /> {/* New Route */}
     </Routes>
   </Router>
 );
