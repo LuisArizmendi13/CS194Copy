194acb7559ba9477bcd99f54f0a4c86c4b3bc3fd
alanaespo <146492343+alanaespo@users.noreply.github.com>
2025-03-11 01:54:48 -0700

    Dish delete added

--- website/src/components/DishList.jsx
+++ website/src/components/DishList.jsx
@@ -1,11 +1,19 @@
+// In DishList.js
 import React from 'react';
 import DishBox from './DishBox';
 
-const DishList = ({ dishes, onArchiveChange, onSaleRecorded }) => {
+const DishList = ({ dishes, onArchiveChange, onSaleRecorded, onDelete }) => {
   return (
-    <div className="mt-4">
-      {dishes.map((dish, index) => (
-        <DishBox key={index} dish={dish} onArchiveChange={onArchiveChange} onSaleRecorded={onSaleRecorded} />
+    <div>
+      {dishes.map((dish) => (
+        <DishBox
+          key={dish.dishId}
+          dish={dish}
+          onDishSelect={() => console.log('Selecting dish')} // Implement onDishSelect if needed
+          isSelected={false} // Implement isSelected if needed
+          isSelectable={false} // Implement isSelectable if needed
+          onDelete={onDelete}
+        />
       ))}
     </div>
   );
